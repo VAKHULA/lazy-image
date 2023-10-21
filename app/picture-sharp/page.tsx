098d38c406044f833eb4823a43aeffef1d36fe90
vakhula <andrii.vakhula@gmail.com>
2023-10-21 12:04:59 +0300

    add picture-sharp

--- app/picture-sharp/page.tsx
+++ app/picture-sharp/page.tsx
@@ -5,22 +5,13 @@ import styles from '../page.module.css'
 import { LazyImageWrapper } from '../LazyImageWrapper'
 
 export default async function Home() {
-  // const response = await fetch('https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg')
-  // const buffer = await response.arrayBuffer()
-  // const  { format, width = 0, height = 0 } = await sharp(buffer).metadata();
-
-  // const resizedImageBuf = await sharp(buffer)
-  //   .resize(Math.round(width * compression), Math.round(height * compression))
-  //   .toBuffer();
-    
   return (
     <main className={styles.main}>
       <picture>
-        <source srcSet="https://lazy-image.vakhula.dev/picture-sharp/api?src=https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg&w=600" media="(min-width: 600px)" />
-        {/* <source srcSet="logo-768.png, logo-768-1.5x.png 1.5x" /> */}
-        {/* <source srcset="photo.avif" type="image/avif" />
-  <source srcset="photo.webp" type="image/webp" /> */}
-        <img src="https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg" alt="MDN" />
+        <source srcSet="https://lazy-image.vakhula.dev/picture-sharp/api?src=https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg&w=1400" media="(min-width: 1000px)" />
+        <source srcSet="https://lazy-image.vakhula.dev/picture-sharp/api?src=https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg&w=1000" media="(min-width: 600px)" />
+        <source srcSet="https://lazy-image.vakhula.dev/picture-sharp/api?src=https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg&w=600" media="(min-width: 300px)" />
+        <img src="https://lazy-image.vakhula.dev/picture-sharp/api?src=https://lazy-image.vakhula.dev/pexels-ioana-motoc-4376217.jpg&w=300" alt="MDN" />
       </picture>
     </main>
   )
